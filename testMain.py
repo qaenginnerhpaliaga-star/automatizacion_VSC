7ba7519f12a80a6f3df3f228ab5acc9c1515520d
Hernan <qa.engineer.hpaliaga@gmail.com>
2026-06-13 23:23:41 -0400

    Agregue testMain

---
+++ testMain.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+#selenium configura el driver automaticamente
+driver = webdriver.Chrome()
+
+#abre pagina web
+driver.get("https://www.google.com")
+time.sleep(10)
+
+print("Selenium funciona correctamente")
+
+#cierra navegador
+driver.quit()
