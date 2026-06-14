0eff0bc333ca72772ac09ab8bb89886c404f9a20
Hernan <qa.engineer.hpaliaga@gmail.com>
2026-06-14 00:52:04 -0400

    Agrego testSet ejecutable master

---
+++ testSet.py
@@ -0,0 +1,9 @@
+import os
+print("Iniciando Test Set Automatizado")
+print("Ejecutando Test Case 1: Abrir Navegador Google")
+os.system("python3 testMain.py")
+print("Test Case 1 Finalizado con exito")
+print("Ejecutando Test Case 2: Realizar Busqueda en Google")
+os.system("python3 buscarMain.py")
+print("Test Case 2 Finalizado con exito")
+print("Test Set Automatizado Finalizado")
